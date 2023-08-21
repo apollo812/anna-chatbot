201b6d08e2e33845db3333ae46f98a528d09483a
apollo812 <businessx.free2000@gmail.com>
2023-08-21 13:21:08 -0400

    feat: development config setting through env file

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-const DEV_MODE = true;
-export const API_BASE = DEV_MODE == true?'http://localhost:3333/api/v1':'';
+const DEV_MODE = process.env.DEV_MODE;
+export const API_BASE = DEV_MODE == true ? 'http://localhost:3333/v1':'https://anna-chatbot-api.onrender.com/v1';
