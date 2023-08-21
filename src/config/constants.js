dfc97dda28d02997b5ea3450f3d6cfcfd075698d
apollo812 <businessx.free2000@gmail.com>
2023-08-21 14:44:41 -0400

    fix: react env config setting

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-const DEV_MODE = process.env.DEV_MODE;
+const DEV_MODE = process.env.REACT_APP_DEV_MODE;
 export const API_BASE = DEV_MODE == true ? 'http://localhost:3333/v1':'https://anna-chatbot-api.onrender.com/v1';
